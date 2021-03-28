fb775104fbea2f6285916d621f7c4f043bcf5d08
Tim H <timh1203@gmail.com>
2021-03-27 21:14:00 -0500

    Add temporary events page

--- pages/events.js
+++ pages/events.js
@@ -1,3 +1,30 @@
+import styled from "styled-components";
+
+const Main = styled.main`
+	text-align: center;
+	a {
+		text-decoration: underline;
+	}
+	span {
+		font-size: 20rem;
+	}
+`;
+
 export default function EventsPage() {
-	return <h1>EventsPage</h1>;
+	return (
+		<Main>
+			<span>👷</span>
+			<h1>
+				To see our latest events, please visit{" "}
+				<a
+					href="https://www.meetup.com/Austin-Hispanic-Hackers-Meetup/"
+					target="_blank"
+				>
+					Meetup.com
+				</a>
+				!
+			</h1>
+			<h2>We're working on our own calendar solution, stay tuned.</h2>
+		</Main>
+	);
 }
