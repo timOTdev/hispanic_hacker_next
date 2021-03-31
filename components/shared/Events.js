d8535315e3796397f52d622c21eedc5644c131fe
Tim H <timh1203@gmail.com>
2021-03-30 22:16:12 -0500

    Update events page with external links

--- components/shared/Events.js
+++ components/shared/Events.js
@@ -1,6 +1,12 @@
 import globals from "../data";
 import styled from "styled-components";
-import { FaMeetup } from "react-icons/fa";
+import {
+	FaMeetup,
+	FaYoutubeSquare,
+	FaFilePowerpoint,
+	FaComments,
+} from "react-icons/fa";
+import Image from "next/image";
 
 const EventsWrapper = styled.aside`
 	display: flex;
@@ -13,9 +19,28 @@ const EventCard = styled.div`
 	justify-content: center;
 	width: 30%;
 	margin: 2rem auto;
+	img {
+		border-radius: 5px;
+	}
+	a {
+		display: block;
+	}
+`;
+const EventCardLinks = styled.div`
+	display: flex;
+	justify-content: space-evenly;
 `;
 const Meetup = styled(FaMeetup)`
 	color: #e61b3e;
+`;
+const Youtube = styled(FaYoutubeSquare)`
+	color: #f70000;
+`;
+const Powerpoint = styled(FaFilePowerpoint)`
+	color: #e86900;
+`;
+const Chat = styled(FaComments)`
+	color: #41abff;
 `;
 
 export default function Events() {
@@ -24,15 +49,52 @@ export default function Events() {
 			{Object.values(globals().eventsHH["2021"]).map((event, index) => (
 				<EventCard key={index}>
 					<h3>{event.month}</h3>
-					<a href={event.meetup} target="_blank">
-						<Meetup size={40} />
-					</a>
+					<Image
+						src={
+							event.titleSlide
+								? `/static/images/${event.titleSlide}`
+								: "/static/images/hyped.png"
+						}
+						alt={event.name + event.date}
+						width={480}
+						height={270}
+					/>
+					<EventCardLinks>
+						{event.meetup ? (
+							<a href={event.meetup} target="_blank">
+								<Meetup size={40} title="Meetup Event Page" />
+							</a>
+						) : (
+							""
+						)}
+						{event.slides ? (
+							<a href={event.slides} target="_blank">
+								<Powerpoint size={40} title="Powerpoint Slides" />
+							</a>
+						) : (
+							""
+						)}
+						{event.youtube ? (
+							<a href={event.youtube} target="_blank">
+								<Youtube size={40} title="Youtube Replay" />
+							</a>
+						) : (
+							""
+						)}
+						{event.zoomChat ? (
+							<a href={event.zoomChat} target="_blank">
+								<Chat size={40} title="Zoom Chat" />
+							</a>
+						) : (
+							""
+						)}
+					</EventCardLinks>
 					<p>
-						<em>{event.name}</em>
+						<u>{event.name}</u>
 						<br />
-						<u>Date</u>: {event.date}
+						<em>Date</em>: {event.date}
 						<br />
-						<u>Time</u>: {event.time}
+						<em>Time</em>: {event.time}
 						<br />
 					</p>
 				</EventCard>
