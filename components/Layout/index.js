84f3a83dd2fef8d0554d2c2b3de837a76d62d68b
timOTdev <tim@timothyhoang.dev>
2022-04-17 16:57:38 -0500

    Fix theme toggle on footer

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -12,7 +12,7 @@ export default function Layout(props) {
     <main>
       <Header {...props} />
       <Section>{props.children}</Section>
-      <Footer />
+      <Footer {...props} />
     </main>
   );
 }
